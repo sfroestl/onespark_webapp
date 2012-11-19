7eb47c9f5c319df0d5ca313ffefa50b0ed418dfb
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-11-19 17:13:17 +0000

    created file for own authenticated REST adapter

--- source/js/app/init.js
+++ source/js/app/init.js
@@ -1,8 +1,1 @@
 App = Ember.Application.create();
-DS.AuthenticatedRESTAdapter = DS.RESTAdapter.extend({
-  ajax: function(url, type, hash) {
-	App.session.insertAuthenticationInRequest(hash);
-    return DS.RESTAdapter.prototype.ajax.call(this,url, type, hash);
-  }
-});
-
