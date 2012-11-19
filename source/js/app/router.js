1cb05d3c6c0021941acc3c92d1da6ddd96655799
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-11-19 00:57:53 +0000

    removed merging artifacts

--- source/js/app/router.js
+++ source/js/app/router.js
@@ -48,51 +48,5 @@ App.Router = Ember.Router.extend({
 	  })
 	})
         
-=======
-
-    loggedIn: Ember.Route.extend({
-      connectOutlets: function(router, context){
-        router.get('applicationController').connectOutlet('session', 'in');
-        router.get('inController').connectOutlet('navigation', 'traversal');
-        router.get('inController').set('sessionUser', App.User.getSessionUser());
-      },
-      goLoggedOut: function(router, evt) {
-        console.log("Try logout");
-        router.get('inController').logout();
-        console.log("Session: " + App.User.signedIn);
-        router.transitionTo('loggedOut');
-
-      },
-        users:  Ember.Route.extend({
-          route: '/users',
-          enter: function ( router ){
-            console.log("The users sub-state was entered.");
-          },
-          connectOutlets: function(router, context){
-          router.get('inController').connectOutlet('body', 'users');
-          router.get('inController').connectOutlet('navigation', 'traversal');
-         }
-        }),
-
-    }),
-
-    loggedOut: Ember.Route.extend({
-      connectOutlets: function(router, context){
-        router.get('applicationController').connectOutlet('session', 'out');
-        router.get('outController').connectOutlet('login', 'loginForm');
-      },
-      goLoggedIn: function(router, evt) {
-        console.log("Try login");
-        router.get('loginFormController').login();
-        console.log("Session: " + App.User.signedIn);
-        if(App.User.signedIn) {
-          router.transitionTo('loggedIn');
-        }
-      }
-    })
->>>>>>> master
-
-
-
   })
 });
