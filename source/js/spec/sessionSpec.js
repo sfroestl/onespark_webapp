f654e98df299c5c0340c2a6ec83919f5f3cc6713
wrobin <wrobin@gmx.net>
2012-11-21 18:32:42 +0100

    adds very raw sessionSpec

--- source/js/spec/sessionSpec.js
+++ source/js/spec/sessionSpec.js
@@ -1,51 +1,37 @@
-/*describe( "The user model", function () {  
+describe( "The session model", function () {  
 
-	var user = null;
+	var session = App.Session.create();
 
     //App = Ember.Application.create();
 
-    beforeEach(function(){
-        user = App.User.create();
-
-    });
-
-    afterEach(function(){
-        user = null;
-    });
-
-	it("should have an get session token function", function(){
-		expect(App.User.getSessionToken).toBeDefined();
+	it("should be defined", function(){
+		expect(session).toBeDefined();
 	});
 
-	it("should have an set session token function", function(){
-		expect(App.User._setSessionToken).toBeDefined();
+	it("should have a login function", function(){
+		expect(session.login).toBeDefined();
 	});
 
-	it("should have an deleteSessionToken function", function(){
-		expect(App.User.deleteSessionToken).toBeDefined();
+	it("should have a logout function", function(){
+		expect(session.login).toBeDefined();
 	});
 
-	it("should have an isSignedIn function", function(){
-		expect(App.User.isSignedIn).toBeDefined();
+	it("should have a insertAuthenticationInRequest function", function(){
+		expect(session.insertAuthenticationInRequest).toBeDefined();
 	});
 
-	it("should have an registerSessionUser function", function(){
-		expect(App.User.registerSessionUser).toBeDefined();
+	it("should have a _storeAsCookie function", function(){
+		expect(session._storeAsCookie).toBeDefined();
 	});
 
-	it("should have an getSessionUser function", function(){
-		expect(App.User.getSessionUser).toBeDefined();
+	it("should have a _findUser function", function(){
+		expect(session._findUser).toBeDefined();
 	});
-
-	it("should have an createUser function", function(){
-		expect(App.User.createUser).toBeDefined();
-	});
-
 
 	describe( "The logged out user", function () {  
 
 		it("should be signed out before login", function(){
-			expect(App.User.signedIn).toEqual(false);
+			expect(session.sessionUser).toEqual(null);
 			//expect(App.User.isSignedIn()).toEqual(false);
 		});
 	});
@@ -53,18 +39,16 @@
 	describe( "The logged in user", function () {  
 
 		beforeEach(function(){
-        	App.User.registerSessionUser("Ym9iOnRlc3Rib2I=");
+        	session.login("bob", "testbob");
 		});
 
 		it("should be signed in after login", function(){
-			expect(App.User.signedIn).toEqual(true);
-			//expect(App.User.isSignedIn()).toEqual(true);
+			console.log("SessionUser ist: " + session.sessionUser);
+			//to Do
 		});
 
 		it("should be signed out again after logout", function(){
-			App.User.deleteSessionToken();
-			expect(App.User.signedIn).toEqual(false);
-			//expect(App.User.isSignedIn()).toEqual(false);
+			//to Do
 		});
 	});
-});*/
+});
