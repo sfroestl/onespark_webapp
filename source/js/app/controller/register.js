442194d0af9b4c7e355f1b193825d9f91e511595
wrobin <wrobin@gmx.net>
2012-11-22 18:45:30 +0100

    spooky register

--- source/js/app/controller/register.js
+++ source/js/app/controller/register.js
@@ -13,8 +13,31 @@ App.RegisterController = Ember.Controller.extend({
     console.log("OutController: Check password: " + this.get("password"));
     console.log("OutController: Check password_confirmation: " + this.get("password_confirmation"));
 
-    //var user = App.store.createRecord(App.User,  { name: this.get("username"), this.get("email"), this.get("password"), this.get("password_confirmation")});
+    //var user = App.store.createRecord(App.User,  { username: this.get("username"), email: this.get("email"), password: this.get("password"), password_confirmation: this.get("password_confirmation")});
+    //App.store.commit();
 
-    console.log("OutController: Check created user: " + user);
+    $.ajax({
+      async: true,
+      url: 'http://api.onespark.de/api/v1/users',
+      type: 'POST',
+      dataType: 'json',
+      accept: 'json',
+      context: this,
+      data: {user:{ username: this.get("username"), email: this.get("email"), password: this.get("password"), password_confirmation: this.get("password_confirmation")}},
+
+      error: function(jqXHR, textStatus){
+        console.log ("--> ERROR");
+      },
+
+      success: function(data) {
+        // store session
+        console.log ("--> Success: 200");
+
+      }
+    });
+
+
+
+    //console.log("OutController: Check created user: " + user);
   }
 });
