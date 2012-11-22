d738bf77e4531760ad1610c503e85f98f776116a
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-11-22 17:17:04 +0000

    finally a workings hasMany association for projects

--- source/js/app/model/user.js
+++ source/js/app/model/user.js
@@ -1,7 +1,11 @@
 App.User = DS.Model.extend({
     username: DS.attr('string'),
     email: DS.attr('string'),
-    ownedProjects: DS.hasMany('App.Project', { key: 'owned_project_ids' }),
-    collaboratedProjects:DS.hasMany('App.Project', { key: 'collaborated_project_ids' }),
+    ownedProjects: DS.hasMany('App.Project'),
+    collaboratedProjects:DS.hasMany('App.Project'),
     profile: DS.belongsTo('App.Profile')
+});
+DS.AuthenticatedRESTAdapter.map('App.User', {
+	ownedProjects: { key: 'owned_project_ids' },
+	collaboratedProjects: { key: 'collaborated_project_ids' }
 });
