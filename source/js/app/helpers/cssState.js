4f30c61bbe10a370039fa3b2feaabc3a6bf50073
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-11-23 17:15:00 +0000

    removed loggin function from css helper

--- source/js/app/helpers/cssState.js
+++ source/js/app/helpers/cssState.js
@@ -1,6 +1,5 @@
 DS.Model.reopen({
 	stateForCSS: function() {
-		console.log(this);
 		var currentState = this.get("stateManager.currentPath");
 		Ember.assert("Unexpected State:" + currentState , currentState.startsWith("rootState."));
 		currentState = currentState.slice(10); //cut away "rootState."
