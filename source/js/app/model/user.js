35f94df88584ebab5400b868842fd17b3d707c57
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-11-24 13:44:18 +0000

    added displayName & displayAvatarUrl Property to User, also (hopefully) corrected binding, which seems to fail never the due a bug in ember-data

--- source/js/app/model/user.js
+++ source/js/app/model/user.js
@@ -5,9 +5,23 @@ App.User = DS.Model.extend({
     password_confirmation: DS.attr('string'),
     ownedProjects: DS.hasMany('App.Project'),
     collaboratedProjects:DS.hasMany('App.Project'),
-    profile: DS.belongsTo('App.Profile')
+    profile: DS.belongsTo('App.Profile'),
+    displayName: function() {
+	  var forename = this.get("profile.forename");
+	  var surname = this.get("profile.surname");
+	  if (forename || surname)
+		   return ((forename)?forename:"") + ((forename && surname)?" ":"")+((surname)?surname:"");
+	  else
+		   return this.get("username");
+	}.property('profile.forename','profile.surname','username'),
+    displayAvatarUrl: function() {
+	  return this.get("profile.avatarURL") || "noavatar.png";
+	}.property('profile.avatarURL')	
 });
 DS.AuthenticatedRESTAdapter.map('App.User', {
 	ownedProjects: { key: 'owned_project_ids' },
-	collaboratedProjects: { key: 'collaborated_project_ids' }
+	collaboratedProjects: { key: 'collaborated_project_ids' },
+	profile: {embedded: true}
 });
+
+
