d8576f5f99431a5c29db1e79dc573d1c932e12ab
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-03 18:37:42 +0000

    example usage of flash messages in NewProjectController

--- source/js/app/controller/newProject.js
+++ source/js/app/controller/newProject.js
@@ -5,11 +5,14 @@ App.NewProjectController = Ember.Controller.extend({
     dueDate: null,
 
     save: function() {
+		var title = this.get("title");
+		var fm = App.FlashMessage.create({text: "Saving Project '" + title +"'",removable:false});
+		var project = App.store.createRecord(App.Project,  { title: title, desc: this.get("description"), owner: this.get("owner"), dueDate: this.get("dueDate")});
+		project.set("didCreate",function() {
+			fm.setProperties({removable: true, text: "Project '" + title + "' saved"});
+		});
+		App.get('session.sessionUser.ownedProjects').addObject(project);
     
-    var project = App.store.createRecord(App.Project,  { title: this.get("title"), desc: this.get("description"), owner: this.get("owner"), dueDate: this.get("dueDate")});
-    App.get('session.sessionUser.ownedProjects').addObject(project);
-    
-    App.store.commit();
-    //project.addObserver('id', this, 'actionToFire');
+		App.store.commit();
 	}
 });
