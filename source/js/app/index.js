ea1c9ed842d49e3bf6473dd0d84440872089fad1
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-03 22:10:59 +0000

    new ember-data version

--- source/js/app/index.js
+++ source/js/app/index.js
@@ -1,6 +1,6 @@
 
 //=require ../libs/ember/ember-1.0.0-pre.2.js
-//=require ../libs/ember-data-rev8.js
+//=require ../libs/ember-data-rev9.js
 //=require ../libs/authenticated_adapter.js
 //=require ./init.js
 //=require_tree ./helpers
