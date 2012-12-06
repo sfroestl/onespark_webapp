edc4ca5d7076c80ace854b7d361383f321885d68
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-06 17:02:35 +0000

    added project coworker name for flashmessage

--- source/js/app/model/projectCoworker.js
+++ source/js/app/model/projectCoworker.js
@@ -1,8 +1,12 @@
 App.ProjectCoworker = DS.Model.extend({
     permission: DS.attr('number'),
     project: DS.belongsTo('App.Project'),
-    user: DS.belongsTo('App.User')
+    user: DS.belongsTo('App.User'),
     //tasks: DS.hasMany('App.Task')
+    flashMessageName: function() {
+		return this.get("user.displayName")+" as coworker";
+	}.property("user.displayName"),
+	
 });
 DS.AuthenticatedRESTAdapter.map('App.Project', {
 	project: { key: 'project_id' },
