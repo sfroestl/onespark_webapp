6528d71ef8a9072a7570ae6a9276dcb09523b6ee
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2012-12-10 13:57:26 +0000

    cleaned unused file

--- source/js/app/helpers/flashMessageState.js
+++
@@ -1,9 +0,0 @@
-DS.Model.reopen({
-	//describe the state of an object for showing it in a flashMessage
-	textForFlashMessage: function() {
-		var currentState = this.get("stateManager.currentPath");
-		Ember.assert("Unexpected State:" + currentState , currentState.startsWith("rootState."));
-		currentState = currentState.slice(10); //cut away "rootState."
-		return currentState.replace(/\./g,"-"); // replace . by "-" to avoid conflicts with css-selectors
-	}.property('stateManager.currentPath')
-});
