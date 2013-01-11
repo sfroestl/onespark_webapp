8840f73c7b6d890a810249a497c763e5a4f54f4c
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-01-11 16:41:44 +0100

    removing of null elements from array properties supresses unloaded elements

--- source/js/app/helpers/ArrayProperties.js
+++ source/js/app/helpers/ArrayProperties.js
@@ -11,7 +11,12 @@ Function.prototype.arrayProperty = function() {
 		meta = Ember.meta(this); //get the meta object of current object
 		if (!(name in meta)) meta[name] = []; 
 		var array = meta[name];
-		array.set("[]",result);
+		console.log(this);
+		console.log(name);
+		console.log(result);
+		var r = result.compact();
+		console.log(realProperty);
+		array.set("[]",r);
 		return array;
 	};
 	return wrapper.property.apply(wrapper,arguments);
