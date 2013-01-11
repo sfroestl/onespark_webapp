fb504d63aa55189bc50a391ca3d85655f4c45dae
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-01-11 16:46:04 +0100

    recreated specific record lists to use generic type

--- source/js/app/view/_projectList.js
+++ source/js/app/view/_projectList.js
@@ -1,6 +1,12 @@
-App.ProjectListView = Ember.View.extend({
-  templateName: 'projectList',
+App.ProjectListView = App.RecordListView.extend({
   title: "Project List",
-  classNames: ['project-list-container'],
-  projects: []
+  image: Ember.View.extend({template: Ember.Handlebars.compile('')}),
+  desc: Ember.View.extend({template: Ember.Handlebars.compile('{{title}}{{#if dueDate}} <span class="project-due-date"> (due {{view "App.FriendlyTimeView" timeBinding="dueDate"}})</span>{{/if}}')}),
+  sortings: [
+    Ember.Object.create({name:"title",sortProperties:["title"]}),
+    Ember.Object.create({name:"due date",sortProperties:["dueDate"]}),
+  ],
+  clickedRecord: function(record){
+    App.router.send("showProject", record);
+  }
 });
