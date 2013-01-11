98ab9d554775d92d8b52e0c1a18d563198899d96
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-01-11 18:23:54 +0100

    fixed graphical errors

--- source/js/app/view/_projectList.js
+++ source/js/app/view/_projectList.js
@@ -1,7 +1,14 @@
 App.ProjectListView = App.RecordListView.extend({
   title: "Project List",
-  image: Ember.View.extend({template: Ember.Handlebars.compile('')}),
-  desc: Ember.View.extend({template: Ember.Handlebars.compile('{{title}}{{#if dueDate}} <span class="project-due-date"> (due {{view "App.FriendlyTimeView" timeBinding="dueDate"}})</span>{{/if}}')}),
+  image: Ember.View.extend({
+	  template: Ember.Handlebars.compile(''),
+	  classNames: ["elem-image"],
+	  tagName: "span"
+  }),
+  desc: Ember.View.extend({template: Ember.Handlebars.compile('{{title}}{{#if dueDate}} <span class="project-due-date"> (due {{view "App.FriendlyTimeView" timeBinding="dueDate"}})</span>{{/if}}'),
+	  classNames: ["elem-desc"],
+	  tagName: "span"
+  }),
   sortings: [
     Ember.Object.create({name:"title",sortProperties:["title"]}),
     Ember.Object.create({name:"due date",sortProperties:["dueDate"]}),
--- source/js/app/view/_userList.js
+++ source/js/app/view/_userList.js
@@ -1,7 +1,14 @@
 App.UserListView = App.RecordListView.extend({
   title: "User List",
-  image: Ember.View.extend({template: Ember.Handlebars.compile('<img {{bindAttr src="record.displayAvatarUrl"}} >')}),
-  desc: Ember.View.extend({template: Ember.Handlebars.compile('{{displayName}}')}),
+  image: Ember.View.extend({
+	  template: Ember.Handlebars.compile('<img {{bindAttr src="record.displayAvatarUrl"}} >'),
+	  classNames: ["elem-image"],
+	  tagName: "span"
+  }),
+  desc: Ember.View.extend({template: Ember.Handlebars.compile('{{displayName}}'),
+	  classNames: ["elem-desc"],
+	  tagName: "span"
+  }),
   sortings: [
     Ember.Object.create({name:"forename",sortProperties:["forenameOrUsername"]}),
     Ember.Object.create({name:"surname",sortProperties:["surnameOrUsername"]}),
