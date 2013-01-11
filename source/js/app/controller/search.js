c2e0813a7d530665c86eec44d3c2a3c9b0c2ff80
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-01-11 17:53:17 +0100

    expanded search to include projects and tasks

--- source/js/app/controller/search.js
+++ source/js/app/controller/search.js
@@ -1,18 +1,24 @@
 App.SearchController = Ember.ObjectController.extend({
 	query: null,
 	allUsers: function() {
-		console.log("owned");
 		var found = new Ember.Set();
 			found.addEach(App.get("session.sessionUser.outContacts").mapProperty("contact"));
-			App.get("session.sessionUser.ownedProjects").forEach(function(project) {
-				console.log(project.get("peopleInvolved"));
+			this.get("allProjects").forEach(function(project) {
 				found.addEach(project.get("peopleInvolved"));
 			});
-			console.log("collaborated");
-			App.get("session.sessionUser.collaboratedProjects").forEach(function(project) {
-				console.log(project.get("peopleInvolved"));
-				found.addEach(project.get("peopleInvolved"));
-			});	
 		return found.toArray();
-	}.arrayProperty("App.session.sessionUser.outContacts.@each.contact","App.session.sessionUser.ownedProjects.@each.peopleInvolved.[]","App.session.sessionUser.collaboratedProjects.@each.peopleInvolved.[]")
+	}.arrayProperty("App.session.sessionUser.outContacts.@each.contact","allProjects.@each.peopleInvolved.[]"),
+	allProjects: function() {
+		var found=[];
+		found.addObjects(App.get("session.sessionUser.ownedProjects"));
+		found.addObjects(App.get("session.sessionUser.collaboratedProjects"));
+		return found;
+	}.arrayProperty("App.session.sessionUser.ownedProjects.[]","App.session.sessionUser.collaboratedProjects.[]"),
+	allTasks: function() {
+		var found=[];
+			this.get("allProjects").forEach(function(project) {
+				found.addObjects(project.get("tasks"));
+			});
+		return found;
+	}.arrayProperty("allProjects.@each.tasks.[]"),	
 })
--- source/templates/search.hb
+++ source/templates/search.hb
@@ -1,8 +1,22 @@
 <p>Search {{view Ember.TextField valueBinding="controller.query"}}</p>
 {{view App.UserListView
   title="Matching Users"
-  usersBinding="controller.allUsers"
+  recordsBinding="controller.allUsers"
   filterBinding="controller.query"
   showFilter=false
-  noUsers="There are no records to display."
-  noFilteredUsers="Your search didn't match any contributor."}}
+  noRecords="There are no users to display."
+  noFilteredRecords="Your search didn't match anyone."}}
+{{view App.ProjectListView
+  title="Matching Projects"
+  recordsBinding="controller.allProjects"
+  filterBinding="controller.query"
+  showFilter=false
+  noRecords="There are no projects to display."
+  noFilteredRecords="Your search didn't match any project."}}
+{{view App.TaskListView
+  title="Matching Tasks"
+  recordsBinding="controller.allTasks"
+  filterBinding="controller.query"
+  showFilter=false
+  noRecords="There are no tasks to display."
+  noFilteredRecords="Your search didn't match any task."}}
