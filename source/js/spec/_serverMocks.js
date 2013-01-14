3e3ef381dd8b87dc046684b98b7c93ae1e33823b
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-01-14 14:31:49 +0100

    DELETE for User, fixed errors

--- source/js/spec/_serverMocks.js
+++ source/js/spec/_serverMocks.js
@@ -10,6 +10,15 @@ var user_1 ={"user":
     "owned_project_ids":[],"collaborated_project_ids":[],"project_coworker_ids":[],"contact_ids":[]
   }
 };
+var user_2 ={"user": 
+  {
+    "id":2,
+    "username":"alice",
+    "email":"alice@testme.com",
+    "profile_id":2,
+    "owned_project_ids":[],"collaborated_project_ids":[],"project_coworker_ids":[],"contact_ids":[]
+  }
+};
 var authenticated_response = function(response) {
 	return function (xhr) {
 	  if (xhr.username!="bob" && xhr.password!="testbob") 
@@ -31,11 +40,15 @@ server.respond("PUT", "http://api.onespark.de/api/v1/users/1",authenticated_resp
 		$.extend(newUser, user_1);
 		return [200,{ "Content-Type": "application/json" },JSON.stringify(newUser)];
 }));
+server.respond("DELETE", "http://api.onespark.de/api/v1/users/1",authenticated_response(function(xhr) {
+		return [204,{ "Content-Type": "application/json" },""];
+}));
+
 server.respond("POST", "http://api.onespark.de/api/v1/users",function(xhr) {
   if (xhr.requestBody.match(/username%5D=bob/)) 
     xhr.respond(422, { "Content-Type": "application/json" }, '{"errors":{"username":["hasalreadybeentaken"]}}');
   else 
-    xhr.respond(200, { "Content-Type": "application/json" }, user_1);
+    xhr.respond(201, { "Content-Type": "application/json" }, JSON.stringify(user_2));
 });
     
 
