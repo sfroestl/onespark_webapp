976bf04f6da2cdcf9f36c91841b4141ef63eb5b3
christian <cs85@me.com>
2013-01-14 20:52:17 +0100

    minor fixes

--- source/js/app/view/_timeSessionList.js
+++ source/js/app/view/_timeSessionList.js
@@ -1,11 +1,12 @@
 App.HistoryTimeSessionListView = App.RecordListView.extend({
   title: "Time Sessions",
   image: Ember.View.extend({template: Ember.Handlebars.compile('')}),
-  desc: Ember.View.extend({template: Ember.Handlebars.compile('{{#if this.end}}From: {{view "App.DateTimeView" timeBinding="start"}}<br/>To: {{view "App.DateTimeView" timeBinding="end"}} ({{this.duration}}){{/if}}')}),
-  sortings: [
-    Ember.Object.create({name:"start",sortProperties:["start"]}),
-    Ember.Object.create({name:"end",sortProperties:["end"]}),
-  ],
+  desc: Ember.View.extend({template: Ember.Handlebars.compile('{{#if this.end}}{{view "App.DateTimeView" timeBinding="start"}} <b>to</b> {{view "App.DateTimeView" timeBinding="end"}} <i>({{this.duration}})</i>{{/if}}')}),
+  // sortings: [
+  //   Ember.Object.create({name:"start",sortProperties:["start"]}),
+  //   Ember.Object.create({name:"end",sortProperties:["end"]}),
+  //   Ember.Object.create({name:"duration",sortProperties:["duration"]}),
+  // ],
   clickedRecord: function(record){
     App.router.send("goToSingleTimeSession", record);
   }
@@ -15,10 +16,11 @@ App.OpenTimeSessionListView = App.RecordListView.extend({
   title: "Time Sessions",
   image: Ember.View.extend({template: Ember.Handlebars.compile('')}),
   desc: Ember.View.extend({template: Ember.Handlebars.compile('{{view "App.DateTimeView" timeBinding="start"}}<button {{action goStopTimeSession}} class="btn-submit">stop</button>')}),
-  sortings: [
-    Ember.Object.create({name:"start",sortProperties:["start"]}),
-    Ember.Object.create({name:"end",sortProperties:["end"]}),
-  ],
+  // sortings: [
+  //   Ember.Object.create({name:"start",sortProperties:["start"]}),
+  //   Ember.Object.create({name:"end",sortProperties:["end"]}),
+  //   Ember.Object.create({name:"duration",sortProperties:["duration"]}),
+  // ],
   clickedRecord: function(record){
     App.router.send("goToSingleTimeSession", record);
   }
