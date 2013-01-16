1456ba58bef9cb5975b6d65db5aac5b1621a54c6
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-01-16 11:44:49 +0100

    fixed login spec

--- source/js/spec/loginSpec.js
+++ source/js/spec/loginSpec.js
@@ -1,7 +1,7 @@
-	xdescribe( "The LoginController as blackbox test", function () {  
+	describe( "The LoginController as blackbox test", function () {  
 
 		var server;
-
+		
 		beforeEach(function() {
 		    this.server = serverMock();
 		    //this.server = sinon.fakeServer.create();
@@ -15,6 +15,7 @@
 		/* login a valid user */
 		it("should run for valid input", function(){
 			
+			console.log("wurst");
 			//this.server.respondWith("GET", "http://api.onespark.de/api/v1/user",
     		//	[200, {"Content-Type": "application/json"},
       		//	'{"user":{"id":2,"username":"bob","email":"bob@testme.com","profile":{"id":2,"forename":"foo","surname":"bar","city":"foobar","about":"here","avatar_url":null,"user_id":2},"owned_project_ids":[4,5,10,11],"collaborated_project_ids":[1,3],"contact_ids":[67,76,54,60,65,63]}}']);
@@ -22,21 +23,22 @@
       		//this.server.respondWith("GET", "http://api.onespark.de/api/v1/users/2",
     		//	[200, {"Content-Type": "application/json"},
       		//	'{"user":{"id":2,"username":"bob","email":"bob@testme.com","profile_id":2,"owned_project_ids":[4,5,10,11],"collaborated_project_ids":[1,3],"project_coworker_ids":[6,1],"outContacts":[67,76,54,60,65,63],"inContacts":[68,75,53,59,66,64]}}']);   
-
-		    loginController.set("username", "bob");
-			loginController.set("password", "testbob");
-			loginController.login();
-
-			//App.session.login("bob","testbob");
-
-		    this.server.respond(); 
-		    this.server.respond();
-		    
-		   	expect(App.get("session.sessionToken")).toEqual(encodeBase64("bob", "testbob"));
-			expect(App.get("session.sessionUser")).not.toBe(null);
-			expect(App.get("session.sessionUserId")).toEqual(2);
-			expect(App.get("session.sessionUser.username")).toEqual("bob");
-			expect(App.get("session.sessionUser.email")).toEqual("bob@testme.com");
+		    runs(function() {
+				loginController.set("username", "bob");
+				loginController.set("password", "testbob");
+				loginController.login();
+			});
+			waitsFor(function() {
+				return App.get("session.signedIn");
+			}, "login", 5000);
+		    runs(function() {
+				console.log("wurst2");			
+				expect(App.get("session.sessionToken")).toEqual(encodeBase64("bob", "testbob"));
+				expect(App.get("session.sessionUser")).not.toBe(null);
+				expect(App.get("session.sessionUserId")).toEqual(1);
+				expect(App.get("session.sessionUser.username")).toEqual("bob");
+				expect(App.get("session.sessionUser.email")).toEqual("bob@testme.com");
+			});
 		})
 
 		/* logout user */
