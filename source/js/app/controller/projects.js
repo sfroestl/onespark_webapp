16361e89bbf529838dc95bb25c6ef247afb03a81
christian <cs85@me.com>
2013-01-20 19:57:10 +0100

    adds validation to editProject

--- source/js/app/controller/projects.js
+++ source/js/app/controller/projects.js
@@ -105,7 +105,7 @@ App.CreateUpdateProjectController = Ember.Controller.extend({
     			}
     		};
     		if((!wrongtime)&&(!wrongdate)&&(!committed)){
-				var project = App.store.createRecord(App.Project,  { title: this.get("title"), desc: this.get("description"), owner: this.get("owner"), dueDate: newDate});
+				var project = App.store.createRecord(App.Project,  { title: this.get("title"), desc: this.get("description"), owner: this.get("owner"), dueDate: null});
 				showFlashMessageFor(project);
 				App.get('session.sessionUser.ownedProjects').addObject(project);
 				App.store.commit();
@@ -153,21 +153,97 @@ App.CreateUpdateProjectController = Ember.Controller.extend({
 
 	update: function(projectToEdit){
 		var project = projectToEdit;
-		project.set("title", this.title);
-		project.set("desc", this.description);
-		var newduedate;
-		var date = Date.parse(this.get("dueDate"));
-		if(isNaN(date)){
-			newduedate=null;
-		}
-		else newduedate= new Date(this.get("dueDate"));
-		project.set("dueDate", newduedate);
-		App.store.commit();
 
-    	var fm = App.FlashMessage.create({
-			text: "Project was updated"
-		});
+		var errorMessageText =null;
+    	var wrongtime =false;
+    	var wrongdate = false;
+    	var newDate = null;
+    	committed = false;
 
-    	//App.router.transitionTo('projects.singleproject.projectOverview');
+    	if((this.get("title")==null)||(this.get("title")=="")){
+    		errorMessageText = "title required";
+
+    		console.log("title required");
+    	}
+    	else {
+    		if(this.get("dueDate")!=null){
+    			var regexDate = "^((?:(?:[1]{1}\\d{1}\\d{1}\\d{1})|(?:[2]{1}\\d{3}))[-:\\/.](?:[0]?[1-9]|[1][012])[-:\\/.](?:(?:[0-2]?\\d{1})|(?:[3][01]{1})))(?![\\d])$";
+    			var validDate = this.get("dueDate").match(regexDate);
+    			if(validDate){
+    				newDate = new Date(this.get("dueDate"));
+    				console.log(newDate);
+    				if(this.get("dueTime")!=null){
+    					var timefield = this.get('dueTime').match(/^([01]?\d|2[0-3]):([0-5]\d)$/);
+    					if(timefield){
+							newDate.setHours(timefield[1]);
+							newDate.setMinutes(timefield[2]);
+							project.set("title", this.get("title"));
+							if((this.get('description')!="")||(this.get("description")!=null)){
+								project.set("desc", this.get("description"));
+							};
+							project.set("dueDate", newDate);
+							App.store.commit();
+							var fm = App.FlashMessage.create({
+								text: "Project was updated"
+							});
+							this.setProperties({
+								title: null,
+								description: null,
+								owner: null,
+								dueDate: null,
+								dueTime: null,
+							});
+							wrongtime = false;
+							wrongdate = false;
+							committed = true;
+							App.router.transitionTo('projectOverview.index', project);
+						}
+						else{
+							errorMessageText = "wrong timeformat";
+							console.log("wrong timeformat");
+							wrongtime = true;
+							this.set("dueTime", null);
+						}
+    				}
+    			}
+    			else{
+    				errorMessageText = "wrong dateformat";
+    				this.set("dueDate", null);
+    				wrongdate = true;
+    				console.log("wrong dateformat");
+    			}
+    		};
+    		if((!wrongtime)&&(!wrongdate)&&(!committed)){
+				project.set("title", this.get("title"));
+				if((this.get('description')!="")||(this.get("description")!=null)){
+					project.set("desc", this.get("description"));
+				};
+				project.set("dueDate", null);
+				App.store.commit();
+				var fm = App.FlashMessage.create({
+					text: "Project was updated"
+				});
+				this.setProperties({
+					title: null,
+					description: null,
+					owner: null,
+					dueDate: null,
+					dueTime: null,
+				});
+				wrongtime = false;
+				wrongdate = false;
+				committed = true;
+				App.router.transitionTo('projectOverview.index', project);
+			}
+    	};
+		if(errorMessageText!=null){
+			var fm = App.FlashMessage.create({
+				text: errorMessageText
+			});
+			errorMessageText = null;
+		};
+		wrongtime = false;
+		wrongdate = false;
+		committed = false;
 	}
 });
