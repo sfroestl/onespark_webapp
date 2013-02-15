98d4baa0ea3111572ed8ff4cc83197826ed9f7d9
Maximilian Rudolph <MaximilianRudolph@gmx.net>
2013-02-15 11:17:29 +0100

    removed unused helper

--- source/js/app/helpers/debug.js
+++
@@ -1,4 +0,0 @@
-Handlebars.registerHelper('debug', function(property, options) {
-  var value = Ember.Handlebars.get(this, property, options);
-  return new Handlebars.SafeString('<span class="highlight">'+value+'</span>');
-});
